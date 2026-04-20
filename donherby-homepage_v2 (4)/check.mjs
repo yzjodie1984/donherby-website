67a9c4525f6d845f470f2e04b3b596b04d246d06
yzjodie1984 <yzjodie@gmail.com>
2026-04-20 16:00:21 +0800

    Delete donherby-homepage_v2 (4) directory

--- donherby-homepage_v2 (4)/check.mjs
+++
@@ -1,7 +0,0 @@
-import https from 'https';
-
-const getUrl = (id) => new Promise((resolve) => {
-  https.get(`https://api.unsplash.com/photos/${id}?client_id=YOURID`, (res) => {
-    // we don't have api key.
-  })
-});
