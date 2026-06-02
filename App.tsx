73c7fdd31204eff974ce6b3ecfc06900c1339c7c
yzjodie1984 <yzjodie@gmail.com>
2026-06-02 13:27:22 +0800

    new cover image

---
+++ App.tsx
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/AboutUs';
+import Services from './pages/Services';
+import Expertise from './pages/Expertise';
+import Contact from './pages/Contact';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService';
+
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen font-sans bg-white text-[var(--color-navy)] selection:bg-[var(--color-navy)] selection:text-white flex flex-col">
+        <Navbar />
+        <div className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/expertise" element={<Expertise />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
---
+++ main.tsx
@@ -0,0 +1,14 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App.tsx';
+import './index.css';
+import './i18n';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>,
+);
